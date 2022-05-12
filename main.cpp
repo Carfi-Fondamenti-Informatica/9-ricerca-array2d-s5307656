63af6e49e5ae0324f3b8d77b25ade4f5bbfad292
s5307656 <100590984+s5307656@users.noreply.github.com>
2022-05-12 12:39:24 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int x;
+    char lista_nomi [10][20];
+    char nome [20];
+
+    init (lista_nomi);
+    init (nome);
+  for (int i=0;i<10;i++){
+      cin >> lista_nomi[i];
+  }
+  cin >> nome;
+  x=ricerca (lista_nomi, nome);
+  if (x!=-1)
+      cout <<x;
+  else cout << "Non presente"<<endl;
+    return 0;
 }
